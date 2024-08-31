0c081242fbd8e7d6edd2ef53738ce1ae35559f7e
lrao <lrao@pop-os.localdomain>
2024-08-31 12:04:01 +0530

    template files added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\admin\AdminLoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/admin/login',[AdminLoginController::class,'index'])->name('admin.login');
